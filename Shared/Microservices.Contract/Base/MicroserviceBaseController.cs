07c5fb9184452db887a9603647f347d869d51713
fatihcandan52 <fatihcandan52@gmail.com>
2021-04-10 13:52:40 +0300

    tüm microservisler için base controller yazıldı dönüş tipleri merkezileştirildi.

---
+++ Shared/Microservices.Contract/Base/MicroserviceBaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microservices.Contract.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Contract.Base
+{
+    public class MicroserviceBaseController : Controller
+    {
+        public IActionResult CreateResult<T>(DataResult<T> result)
+        {
+            return new ObjectResult(result)
+            {
+                StatusCode = (int)result.StatusCode
+            };
+        }
+
+        public IActionResult CreateResult(MessageResult result)
+        {
+            return new ObjectResult(result)
+            {
+                StatusCode = (int)result.StatusCode
+            };
+        }
+    }
+}

adad861eb7390858575a7fffdf119f29fcff46fb
fatihcandan52 <fatihcandan52@gmail.com>
2021-04-11 20:10:40 +0300

    identity server 4 projesi eklendi

---
+++ IdentityServer/Microservices.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microservices.Contract.Result;
+using Microservices.IdentityServer.Dtos;
+using Microservices.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.IdentityServer.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+                City = signUpDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(MessageResult.Error(result.Errors.Select(x => x.Description).ToList(), Contract.Enums.StatusCode.Failed));
+            }
+
+            return Ok(MessageResult.Success(Contract.Enums.StatusCode.OkNoContent));
+        }
+    }
+}
--- Shared/Microservices.Contract/Enums/StatusCode.cs
+++ Shared/Microservices.Contract/Enums/StatusCode.cs
@@ -5,5 +5,6 @@
         Ok = 200, //Dönüş Body si olan başarılı kayıt
         OkNoContent = 204, //Dönüş Body si olmayan başarılı kayıt
         NotFound = 404, // Kayıt bulunamadığında dönüş
+        Failed = 400, // İşlem başarısız olduğunda
     }
 }
